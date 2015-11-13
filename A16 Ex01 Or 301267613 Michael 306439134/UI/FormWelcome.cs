e7b8dd20bc1be73243921cf3705ad577589a0ad1
Or <or@yagel.me>
2015-11-13 09:43:34 +0200

    edit ID

---
+++ A16 Ex01 Or 301267613 Michael 306439134/UI/FormWelcome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Facebook;
+using FacebookWrapper;
+
+namespace UI
+{
+    public partial class FormWelcome : Form
+    {
+        public FormWelcome()
+        {
+            InitializeComponent();
+        }
+
+        private void loginAndInit()
+        {
+            LoginResult result = FacebookService.Login
+        }
+    }
+}
